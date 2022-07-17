c506f26478a968e3155b50e80970136e7c1502d7
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-07-17 23:53:50 +0300

    Initial Commit

---
+++ iExpence/iExpenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenceApp.swift
+//  iExpence
+//
+//  Created by newbie on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
